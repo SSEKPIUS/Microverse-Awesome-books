7cf9c043a049ba8ee1b02e37d98c96daf6c00f26
pius ssekweyama <ssekweyamapius@gmail.com>
2022-11-01 09:51:39 +0300

    nav actions

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+function toggleView(index) {
+  switch (index) {
+    case 0:
+      document.getElementById('list').style.display = 'flex';
+      document.getElementById('new').style.display = 'none';
+      document.getElementById('contact').style.display = 'none';
+      break;
+    case 1:
+      document.getElementById('list').style.display = 'none';
+      document.getElementById('new').style.display = 'flex';
+      document.getElementById('contact').style.display = 'none';
+      break;
+    case 2:
+      document.getElementById('list').style.display = 'none';
+      document.getElementById('new').style.display = 'none';
+      document.getElementById('contact').style.display = 'flex';
+      break;
+    default:
+      document.getElementById('list').style.display = 'flex';
+      document.getElementById('new').style.display = 'none';
+      document.getElementById('contact').style.display = 'none';
+  }
+}
 window.addEventListener('DOMContentLoaded', () => {
-  /* js content here */
+  document.getElementById('list-link').addEventListener('click', (e) => {
+    e.target.style.color = 'blue';
+    document.querySelector('#new-link span').style.color = 'inherit';
+    document.querySelector('#contact-link span').style.color = 'inherit';
+    toggleView(0);
+  });
+  document.getElementById('new-link').addEventListener('click', (e) => {
+    e.target.style.color = 'blue';
+    document.querySelector('#list-link span').style.color = 'inherit';
+    document.querySelector('#contact-link span').style.color = 'inherit';
+    toggleView(1);
+  });
+  document.getElementById('contact-link').addEventListener('click', (e) => {
+    e.target.style.color = 'blue';
+    document.querySelector('#list-link span').style.setProperty('color', 'inherit');
+    document.querySelector('#new-link span').style.setProperty('color', 'inherit');
+    toggleView(2);
+  });
 });
